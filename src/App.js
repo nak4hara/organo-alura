2d3f985aa4cdbf69d87c1dd7f6148e63f4e25996
Karina <lkarina.nakahara@gmail.com>
2023-06-29 10:37:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Banner from './components/Banner/Banner'
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export default function Banner() {
+    return (
+        <div className='banner'>
+            <img src='/images/banner.png' alt='Banner da página' />
+        </div>
+    )
+}
